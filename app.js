a32c3e2b0e43507e372384f2a39c2b720b6963e7
Ethan Barker <ethanbarker0@gmail.com>
2017-09-11 11:19:15 -0700

    Working lobby and rooms

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const htmlPath = path.join(__dirname, '/public');
 app.use(express.static(htmlPath));
 
 
+let rooms = {};
+
 io.on('connection', function(socket){
     console.log('a user connected');
     socket.emit("Start_Chat");
@@ -26,4 +28,59 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
     });
+
+    socket.on("create-room", function (msg) {
+        let room = new Room(msg);
+        rooms[room.code] = room;
+        socket.on("username", function (username) {
+            room.users.push(username);
+            console.log(username + "created room: " + room.code);
+            room.socketRoom.emit("username", room.users);
+        });
+
+        socket.emit("confirm-room", room.code);
+    });
+
+    socket.on("join-room", function (msg) {
+        if (msg in rooms) {
+            socket.emit("room-joined", "joined");
+            socket.on("username", function (username) {
+                rooms[msg].users.push(username);
+                console.log(username + "joined room: " + rooms[msg].code);
+                rooms[msg].socketRoom.emit("username", rooms[msg].users);
+            });
+        } else {
+            socket.emit("room-joined", "bad");
+        }
+    })
 });
+
+function Room(owner) {
+    this.code = GenerateCode();
+    this.users = [];
+    this.owner = owner;
+
+    this.socketRoom = io.of('/' + this.code);
+    // this.socketRoom.on("username", function (msg) {
+    //     this.users.push(msg);
+    //     console.log(this.users);
+    // });
+
+    this.checkCode = function(toCheck) {
+        return toCheck === this.code;
+
+    }
+}
+
+/**
+ * @return {string}
+ */
+function GenerateCode() {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
--- public/main.js
+++ public/main.js
@@ -1,7 +1,19 @@
 const socket = io.connect(window.location.host);
-let join, create;
+let join, create, myName;
 
 let newPress = true;
+
+const scenes = {
+    MAIN: 0,
+    ROOM: 1,
+    GAME: 2
+};
+
+let currentScene = scenes.MAIN;
+
+let code;
+
+let players = [];
 
 function setup() {
     console.log("ye");
@@ -15,16 +27,55 @@ function setup() {
 
     });
     create = new Button("Create Room", 200, 40, width/2 - 100, height/2, function () {
-        alert("");
+        myName = prompt("What's your name?");
+        socket.emit("create-room", myName);
+        socket.on("confirm-room", function (msg) {
+            code = msg;
+            currentScene = scenes.ROOM;
+            let roomSocket = io('/' + code);
+            roomSocket.on("username", function (msg) {
+                players = msg;
+            });
+            socket.emit("username", myName);
+        });
     });
     join = new Button("Join Room", 200, 40, width/2 + 100, height/2, function () {
-        alert("");
+        code = prompt("What's the room code?");
+        socket.emit("join-room", code);
+        socket.on("room-joined", function (msg) {
+            if (msg === "joined") {
+                let roomSocket = io('/' + code);
+                roomSocket.on("username", function (msg) {
+                    players = msg;
+                });
+                myName = prompt("Room found, what's your name?");
+                socket.emit("username", myName);
+                currentScene = scenes.ROOM;
+            } else {
+                alert("Room not found")
+            }
+        })
     });
 }
 
 function draw() {
-    join.tick();
-    create.tick();
+    background(255);
+    if (currentScene === scenes.MAIN) {
+        join.tick();
+        create.tick();
+    } else if (currentScene === scenes.ROOM) {
+        textSize(40);
+        textAlign(CENTER, CENTER);
+        color(0, 0, 0);
+        text("Join with code: " + code, width/2, height/6);
+        text("Connected Users: ", width/2, height/6 + 80);
+
+
+        textSize(20);
+        for (let index = 0; index < players.length; ++index) {
+            text(players[index], width/2, height/6 + 120 + (index * height/20));
+        }
+    }
 }
 
 function Button(label, width, height, x, y, onClick) {
@@ -42,7 +93,6 @@ function Button(label, width, height, x, y, onClick) {
         line(this.x + this.width/2, this.y + this.height/2, this.x - this.width/2, this.y + this.height/2);
         line(this.x - this.width/2, this.y + this.height/2, this.x - this.width/2, this.y - this.height/2);
 
-        
         textSize(30);
         textAlign(CENTER, CENTER);
         color(0, 0, 0);
